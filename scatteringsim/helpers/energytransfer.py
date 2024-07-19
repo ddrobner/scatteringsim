b2b35185f51479718e80e034f509f886a576d39f
David Drobner <david@drobner.ca>
2024-07-19 16:40:50 -0400

    Fix and clean up energy transfer

--- scatteringsim/helpers/energytransfer.py
+++ scatteringsim/helpers/energytransfer.py
@@ -2,30 +2,15 @@ from scatteringsim.structures import ScatterFrame
 import numpy as np
 
 def energy_transfer(e_alpha, scatter_angle):
-    m_alpha = 6.646E-27 # kg
-    m_proton = 1.6726E-27 # kg
+    m_alpha = np.float128(6.646E-27) # kg
+    m_proton = np.float128(1.6726E-27) # kg
 
-    mev_to_j = 1.602176e-13
-    ei_j = e_alpha * mev_to_j
+    e_alpha = np.float128(e_alpha)
 
-    # just identifying scatter_angle as theta_1 to match the algebra
-    # this is in the lab frame
     Theta = scatter_angle
 
-    # and now we determine the final velocity of the alpha in the lab frame
+    frac_energy = (np.power(m_alpha, 2) + 2*m_alpha*m_proton*np.cos(Theta) + np.power(m_proton, 2))/(np.power(m_alpha + m_proton, 2)) - 1
+    ealpha_f = e_alpha*frac_energy + e_alpha
+    eproton_f = np.abs(e_alpha*frac_energy)
 
-    # first thing we need to do is to work out the velocity of the alpha
-    # particle in the lab frame
-    # note that the initial proton velocity in the lab frame is 0
-    frac_energy = 2*m_alpha*m_proton*(np.cos(Theta) - 1)/((m_alpha + m_proton)**2)
-    valpha_i = np.sqrt(2*ei_j/m_alpha)
-    valpha_f = valpha_i * frac_energy
-    vproton_f = (1-frac_energy)*valpha_i
-
-    eproton_f = (1/mev_to_j)*0.5*m_proton*(vproton_f**2)
-    ealpha_f = (1/mev_to_j)*0.5*m_alpha*(valpha_f**2)
-
-    #theta_1 = np.arctan(m_proton*np.sin(Theta)/(np.cos(Theta) - (m_alpha/m_proton)))
-
-    #return (ealpha_f, eproton_f, Theta)
     return ScatterFrame(ealpha_f, eproton_f, Theta)
