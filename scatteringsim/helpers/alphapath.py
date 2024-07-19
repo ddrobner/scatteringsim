5ad74fa46a22ce876a225fea27bf97f2926ae36d
David Drobner <david@drobner.ca>
2024-07-19 16:39:53 -0400

    Return alpha path as ndarray

--- scatteringsim/helpers/alphapath.py
+++ scatteringsim/helpers/alphapath.py
@@ -1,9 +1,11 @@
 from scatteringsim.helpers.stoppingpower import stp_interp
+import numpy as np
+import numpy.typing as npt
 
-def gen_alpha_path(e_0, stp, epsilon=0.1, stepsize=0.001) -> list[float]:
+def gen_alpha_path(e_0, stp, epsilon=0.1, stepsize=0.001) -> npt.NDArray[np.float64]:
     e_i = e_0
     alpha_path = []
     while e_i > epsilon:
         alpha_path.append(e_i)
         e_i = e_i - stp_interp(e_i, stp)*stepsize
-    return alpha_path
+    return np.array(alpha_path)
