a68f1ddb32d4a796568257dea588c37f2df66517
David Drobner <david@drobner.ca>
2024-08-02 15:31:53 -0400

    Start work on script converting cross section to a root format

--- csv2hist.py
+++ csv2hist.py
@@ -1,2 +1,20 @@
-import uproot
 import pandas as pd
+import numpy as np
+import uproot
+
+from hist import Hist
+
+import sys
+
+# run this script by calling python csv2hist.py <input_filename>.csv <output_filename>.root
+
+cx = pd.read_csv(sys.argv[1])
+# converting to radians
+cx['theta'] = np.deg2rad(cx['theta'])
+cx['cx'] = 180/np.pi * cx['cx']
+
+
+
+
+#with uproot.recreate(sys.argv[1]) as f:
+    
