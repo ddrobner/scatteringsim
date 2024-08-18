2fcc56dab919157b70f0eac20acba9c36fd01539
David Drobner <david@drobner.ca>
2024-08-18 15:26:58 -0400

    Add new steps to gpu run script

--- run_gpu.py
+++ run_gpu.py
@@ -1,4 +1,8 @@
 from scatteringsim import GPUSim
 
-s = GPUSim(8.0, 10000, 1E-6, 200, "stoppingpowers/lab.csv", "crossections/combined_new3.csv")
-print(s.particle_sim())
+s = GPUSim(8.0, 100, 1E-6, 200, "stoppingpowers/lab.csv", "crossections/combined_new3.csv")
+s.particle_sim()
+s.quenched_spectrum()
+s.detsim()
+
+print(s.result())
