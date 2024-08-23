3844e8c3c8c3f6b8e9fa14e090b4ec2d6947217d
David Drobner <david@drobner.ca>
2024-08-23 13:14:28 -0400

    leg

--- test_inverse_dist.py
+++ test_inverse_dist.py
@@ -15,10 +15,13 @@ sample_counts_norm = np.divide(sample_counts, sample_counts.max())
 dist_points = s.get_cx(5.3, 1000)
 dist_points_norm = dist_points/np.max(dist_points)
 
+print(dist_points_norm)
+
 fig, ax = plt.subplots()
 
 ax.hist(sample_bins[:-1], sample_bins, weights=sample_counts_norm, label='Inv. Dist')
 ax.scatter(np.linspace(*s.angles, 1000), dist_points_norm, color='red', label='Actual Points')
+fig.legend()
 ax.set_xlabel("Theta")
 ax.set_ylabel("Dist (arb.)")
 
