38c65e601e0730985c6bfbbf6ebcadea9429c40a
David Drobner <david@drobner.ca>
2024-08-23 13:13:33 -0400

    Upd inv dist test

--- test_inverse_dist.py
+++ test_inverse_dist.py
@@ -8,7 +8,7 @@ plt.rcParams['figure.figsize'] = (12, 8)
 s = GPUSim(5.3, 10000, 1E-6, 200, "stoppingpowers/lab.csv", "crossections/combined_new3.csv", proton_factor=0.5)
 
 
-samp = s.gen_dist_samples(5.3, int(1E6))
+samp = s.gen_dist_samples(5.3, int(1000))
 sample_counts, sample_bins = np.histogram(samp, 50)
 sample_counts_norm = np.divide(sample_counts, sample_counts.max())
 
@@ -17,8 +17,8 @@ dist_points_norm = dist_points/np.max(dist_points)
 
 fig, ax = plt.subplots()
 
-ax.hist(sample_bins[:-1], sample_bins, weights=sample_counts_norm)
-ax.plot(np.linspace(*s.angles, 1000), dist_points_norm)
+ax.hist(sample_bins[:-1], sample_bins, weights=sample_counts_norm, label='Inv. Dist')
+ax.scatter(np.linspace(*s.angles, 1000), dist_points_norm, color='red', label='Actual Points')
 ax.set_xlabel("Theta")
 ax.set_ylabel("Dist (arb.)")
 
