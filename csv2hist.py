60c892e89ea21ca4c2cf722ee9e3f697cec71588
David Drobner <david@drobner.ca>
2024-09-24 00:04:10 -0400

    Update csv2hist script

--- csv2hist.py
+++ csv2hist.py
@@ -3,20 +3,30 @@ import numpy as np
 import uproot
 import matplotlib.pyplot as plt
 
+from scatteringsim import GPUSim
+
 import sys
 
-# run this script by calling python csv2hist.py <input_filename>.csv <output_filename>.root
+# run this script by calling python csv2hist.py <input_filename>.csv
+# <output_filename>.root
 
-cx = pd.read_csv(sys.argv[1])
-# converting to radians
-cx['theta'] = np.deg2rad(cx['theta'])
-# TODO figure this out
-#cx['cx'] = 180/np.pi * cx['cx']
+# all properties aside from CX are irrelevant here
+s = GPUSim(5.3, 1000, 1E-6, 200, "stoppingpowers/lab.csv", sys.argv[1])
 
-n_xbins = len(cx['energy'].unique())
-n_ybins = len(cx['theta'].unique())
+# read cx from the GPUSim obj
+cx = s.diff_cx
 
-h = np.histogram2d(cx['energy'].to_numpy(), cx['theta'].to_numpy(), weights=cx['cx'].to_numpy(), bins=[n_xbins, n_ybins])
+n_bins = 10000
+energy_vals = np.linspace(*s.energy_range, n_bins)
+angle_vals = np.linspace(*s.angle_range, n_bins)
+
+cx_vals = []
+
+for e in energy_vals:
+    for a in angle_vals:
+        cx_vals.append(s.interpolator((e, a))) 
+
+h = np.histogram2d(energy_vals, angle_vals, weights=cx_vals, bins=[n_bins, n_bins])
 
 with uproot.recreate(sys.argv[2]) as f:
     f['cx'] = h
